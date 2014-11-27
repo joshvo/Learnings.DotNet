182e95523466f04f232ff44a98f790245156d2c6
JoshVo <luckystar.silver@gmail.com>
2014-11-27 10:47:12 +0700

    Add new solution

---
+++ Learnings.DotNet.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Learnings.DotNet.Web.Startup))]
+namespace Learnings.DotNet.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
